2f75e0569572a0cf1f46ef7fde6cd3b910061385
Kirill <bas@gmail.com>
2025-11-05 15:57:24 +0300

    added logic FileOutput

--- src/main/java/org/example/fileio/FileOutput.java
+++ src/main/java/org/example/fileio/FileOutput.java
@@ -1,4 +1,24 @@
 package org.example.fileio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileOutput {
+    public static List<String> readFile(String path) {
+        try {
+            Path pathNorm = Paths.get(path);
+            if (!Files.exists(pathNorm)) {
+                System.out.println("Файл не найден: " + path);
+                return List.of();
+            }
+
+            return Files.readAllLines(pathNorm);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return List.of();
+    }
 }
